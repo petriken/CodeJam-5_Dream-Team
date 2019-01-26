b28765af547468b2c2296f5cbf605962a501c8e9
Yana Bashtyk <yankabashtyk16@gmail.com>
2019-01-27 02:06:36 +0300

    feat: add main page layout with team members cards

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,17 +1,107 @@
 import React, { Component } from 'react';
 import { Translate } from 'react-redux-i18n';
+import {
+  Card,
+  CardBody,
+  CardText,
+  CardLink,
+  CardTitle,
+  Jumbotron,
+  Container,
+  Row,
+  Col,
+} from 'reactstrap';
 
 class HomePage extends Component {
   // eslint-disable-next-line class-methods-use-this
   render() {
     return (
-      <div>
+      <div style={{ marginBottom: '20px' }}>
           <div>
-            <Translate value="application.title" />
+            <Row>
+              <Col sm="8" md="6" lg="6">
+              <Jumbotron fluid style={{ backgroundColor: '#4fcac24d' }}>
+              <Container fluid>
+                <h1 className="display-6"><Translate value="intro.title" /></h1>
+                <p className="lead"><Translate value="intro.text" /></p>
+                <p className="lead">
+                  <a className="btn btn-primary btn-lg btn-block" style={{ backgroundColor: '#007bff69' }} href="/producers"><Translate value="intro.start"/></a>
+                </p>
+              </Container>
+            </Jumbotron>
+            </Col>
+            <Col sm="8" md="6" lg="6">
+            <h2><Translate value="intro.news"/></h2>
+              <Card body>
+              <Row>
+                <Col sm="6" md="6">
+                <img width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=318" alt="Card image cap" />
+                </Col>
+                <Col sm="6" md="6">
+                <CardBody>
+                  <CardTitle>Card title</CardTitle>
+                  <CardText>Some quick example text to build on the card title.</CardText>
+                </CardBody>
+                </Col>
+              </Row>
+              </Card>
+            </Col>
+            </Row>
           </div>
-          <div>
-            <Translate value="application.text" />
-          </div>
+          <h3><Translate value="intro.devs"/></h3>
+          <Row>
+          <Col sm="6" md="6" lg="4">
+            <Card body text-center inverse style={{ backgroundColor: '#d6effc80', marginBottom: '20px' }}>
+              <img width="100%" src="https://brateckrolikstudent.github.io/rsschool-codejam1-cv/images/avatar.jpg" alt="Avatar photo of a team member" />
+              <CardBody>
+                <CardTitle style={{ color: '#010b15' }}><h4><Translate value="team.member1.name"/></h4></CardTitle>
+                <CardTitle style={{ color: '#010b15' }}>Github: <CardLink href="https://github.com/brateckrolikstudent" target="_blank">brateckrolikstudent</CardLink></CardTitle>
+                <CardTitle style={{ color: '#010b15' }}>Email: <CardLink href="mailto:ibelieveinarabbit@gmail.com">ibelieveinarabbit@gmail.com</CardLink></CardTitle>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col sm="6" md="6" lg="4">
+           <Card body inverse style={{ backgroundColor: '#d6effc80', marginBottom: '20px' }}>
+              <img width="100%" src="https://anastasiyamazura.github.io/rsschool-codejam1-cv/img/Mazura_Anastasia.jpg" alt="Avatar photo of a team member" />
+              <CardBody>
+              <CardTitle style={{ color: '#010b15' }}><h4><Translate value="team.member2.name"/></h4></CardTitle>
+              <CardTitle style={{ color: '#010b15' }}>Github: <CardLink href="https://github.com/AnastasiyaMazura" target="_blank">AnastasiyaMazura</CardLink></CardTitle>
+              <CardTitle style={{ color: '#010b15' }}>Email: <CardLink href="mailto:mazura.anastasiya@gmail.com">mazura.anastasiya@gmail.com</CardLink></CardTitle>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col sm="6" md="6" lg="4">
+           <Card body inverse style={{ backgroundColor: '#d6effc80', marginBottom: '20px' }}>
+              <img width="100%" src="https://avatars2.githubusercontent.com/u/41955026?s=460&v=4" alt="Avatar photo of a team member" />
+              <CardBody>
+              <CardTitle style={{ color: '#010b15' }}><h4><Translate value="team.member3.name"/></h4></CardTitle>
+              <CardTitle style={{ color: '#010b15' }}>Github: <CardLink href="https://github.com/dzianiskavalionak" target="_blank">dzianiskavalionak</CardLink></CardTitle>
+              <CardTitle style={{ color: '#010b15' }}>Email: <CardLink href="mailto:nereidy4@gmail.com">nereidy4@gmail.com</CardLink></CardTitle>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col sm="6" md="6" lg="4">
+           <Card body inverse style={{ backgroundColor: '#d6effc80', marginBottom: '20px' }}>
+              <img width="100%" src="https://cdn.discordapp.com/attachments/537907151985704962/538789462943989773/photo.png" size="sm" alt="Avatar photo of a team member" />
+              <CardBody>
+              <CardTitle style={{ color: '#010b15' }}><h4><Translate value="team.member4.name"/></h4></CardTitle>
+              <CardTitle style={{ color: '#010b15' }}>Github: <CardLink href="https://github.com/petriken" target="_blank">petriken</CardLink></CardTitle>
+              <CardTitle style={{ color: '#010b15' }}>Email: <CardLink href="mailto:petriken82@gmail.com">petriken82@gmail.com</CardLink></CardTitle>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col sm="6" md="6" lg="4">
+           <Card body inverse style={{ backgroundColor: '#d6effc80', marginBottom: '20px' }}>
+              <img width="100%" src="https://scontent-frx5-1.xx.fbcdn.net/v/t1.0-9/27973479_1736578996406233_261896360789868839_n.jpg?_nc_cat=107&_nc_ht=scontent-frx5-1.xx&oh=d49a9311ec8afaf37cb07a9ccec51f96&oe=5CB57C44"
+              alt="Avatar photo of a team member" />
+              <CardBody>
+                <CardTitle style={{ color: '#010b15' }}><h4><Translate value="team.member5.name"/></h4></CardTitle>
+                <CardTitle style={{ color: '#010b15' }}>Github: <CardLink href="https://github.com/yana-bashtyk" target="_blank">yana-bashtyk</CardLink></CardTitle>
+                <CardTitle style={{ color: '#010b15' }}>Email: <CardLink href="mailto:yankabshtyk16@gmail.com">yankabshtyk16@gmail.com</CardLink></CardTitle>
+              </CardBody>
+            </Card>
+          </Col>
+          </Row>
       </div>
     );
   }
--- src/services/store.js
+++ src/services/store.js
@@ -22,6 +22,30 @@ const translationsObject = {
       title: 'Hello World',
       text: 'This is a simple message',
     },
+    intro: {
+      title: 'Film directors of Belarus',
+      text: 'Welcome to the cultural portal, where you will find information about Belarusian film directors. Photos, facts from life, videos and more!',
+      news: 'Director of the day',
+      start: 'Start',
+      devs: 'Developers of the website',
+    },
+    team: {
+      member1: {
+        name: 'Dmitriy Kunets',
+      },
+      member2: {
+        name: 'Anastasiya Mazura',
+      },
+      member3: {
+        name: 'Dzianis Kavalionak',
+      },
+      member4: {
+        name: 'Piotr Stashukevich',
+      },
+      member5: {
+        name: 'Yana Bashtyk',
+      },
+    },
     authors: {
       author1: {
         firstName: 'Ivan',
@@ -38,9 +62,33 @@ const translationsObject = {
       title: 'Привет мир',
       text: 'Это простое сообщение',
     },
+    intro: {
+      title: 'Режиссёры кино Беларуси',
+      text: 'Добро пожаловать на культурный портал, где вы найдете информацию о белорусских режиссёрах кино. Фотографии, факты из жизни, видео и многое другое!',
+      news: 'Режиссёр дня',
+      start: 'Начать',
+      devs: 'Разработчики сайта',
+    },
+    team: {
+      member1: {
+        name: 'Дмитрий Кунец',
+      },
+      member2: {
+        name: 'Анастасия Мазура',
+      },
+      member3: {
+        name: 'Денис Ковалёнок',
+      },
+      member4: {
+        name: 'Пётр Сташукевич',
+      },
+      member5: {
+        name: 'Яна Баштык',
+      },
+    },
     pages: {
       home: 'Главная',
-      producers: 'Режиссеры',
+      producers: 'Режиссёры',
     },
     authors: {
       author1: {
