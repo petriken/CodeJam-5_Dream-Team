84222a5381c6b07582eb34cc1202a39ff30700f5
Yana Bashtyk <yankabashtyk16@gmail.com>
2019-01-27 04:35:33 +0300

    refactor: make card component to reuse identic code

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Translate } from 'react-redux-i18n';
+import { connect } from 'react-redux';
 import {
   Card,
   CardBody,
@@ -11,10 +12,14 @@ import {
   Row,
   Col,
 } from 'reactstrap';
+import PropTypes from 'prop-types';
 
 class HomePage extends Component {
   // eslint-disable-next-line class-methods-use-this
   render() {
+    const {
+      translations,
+    } = this.props;
     return (
       <div style={{ marginBottom: '20px' }}>
           <div>
@@ -50,61 +55,50 @@ class HomePage extends Component {
           </div>
           <h3><Translate value="intro.devs"/></h3>
           <Row>
-          <Col sm="6" md="6" lg="4">
-            <Card body text-center inverse style={{ backgroundColor: '#d6effc80', marginBottom: '20px' }}>
-              <img width="100%" src="https://brateckrolikstudent.github.io/rsschool-codejam1-cv/images/avatar.jpg" alt="Avatar photo of a team member" />
-              <CardBody>
-                <CardTitle style={{ color: '#010b15' }}><h4><Translate value="team.member1.name"/></h4></CardTitle>
-                <CardTitle style={{ color: '#010b15' }}>Github: <CardLink href="https://github.com/brateckrolikstudent" target="_blank">brateckrolikstudent</CardLink></CardTitle>
-                <CardTitle style={{ color: '#010b15' }}>Email: <CardLink href="mailto:ibelieveinarabbit@gmail.com">ibelieveinarabbit@gmail.com</CardLink></CardTitle>
-              </CardBody>
-            </Card>
-          </Col>
-          <Col sm="6" md="6" lg="4">
-           <Card body inverse style={{ backgroundColor: '#d6effc80', marginBottom: '20px' }}>
-              <img width="100%" src="https://anastasiyamazura.github.io/rsschool-codejam1-cv/img/Mazura_Anastasia.jpg" alt="Avatar photo of a team member" />
-              <CardBody>
-              <CardTitle style={{ color: '#010b15' }}><h4><Translate value="team.member2.name"/></h4></CardTitle>
-              <CardTitle style={{ color: '#010b15' }}>Github: <CardLink href="https://github.com/AnastasiyaMazura" target="_blank">AnastasiyaMazura</CardLink></CardTitle>
-              <CardTitle style={{ color: '#010b15' }}>Email: <CardLink href="mailto:mazura.anastasiya@gmail.com">mazura.anastasiya@gmail.com</CardLink></CardTitle>
-              </CardBody>
-            </Card>
-          </Col>
-          <Col sm="6" md="6" lg="4">
-           <Card body inverse style={{ backgroundColor: '#d6effc80', marginBottom: '20px' }}>
-              <img width="100%" src="https://avatars2.githubusercontent.com/u/41955026?s=460&v=4" alt="Avatar photo of a team member" />
-              <CardBody>
-              <CardTitle style={{ color: '#010b15' }}><h4><Translate value="team.member3.name"/></h4></CardTitle>
-              <CardTitle style={{ color: '#010b15' }}>Github: <CardLink href="https://github.com/dzianiskavalionak" target="_blank">dzianiskavalionak</CardLink></CardTitle>
-              <CardTitle style={{ color: '#010b15' }}>Email: <CardLink href="mailto:nereidy4@gmail.com">nereidy4@gmail.com</CardLink></CardTitle>
-              </CardBody>
-            </Card>
-          </Col>
-          <Col sm="6" md="6" lg="4">
-           <Card body inverse style={{ backgroundColor: '#d6effc80', marginBottom: '20px' }}>
-              <img width="100%" src="https://cdn.discordapp.com/attachments/537907151985704962/538789462943989773/photo.png" size="sm" alt="Avatar photo of a team member" />
-              <CardBody>
-              <CardTitle style={{ color: '#010b15' }}><h4><Translate value="team.member4.name"/></h4></CardTitle>
-              <CardTitle style={{ color: '#010b15' }}>Github: <CardLink href="https://github.com/petriken" target="_blank">petriken</CardLink></CardTitle>
-              <CardTitle style={{ color: '#010b15' }}>Email: <CardLink href="mailto:petriken82@gmail.com">petriken82@gmail.com</CardLink></CardTitle>
-              </CardBody>
-            </Card>
-          </Col>
-          <Col sm="6" md="6" lg="4">
-           <Card body inverse style={{ backgroundColor: '#d6effc80', marginBottom: '20px' }}>
-              <img width="100%" src="https://scontent-frx5-1.xx.fbcdn.net/v/t1.0-9/27973479_1736578996406233_261896360789868839_n.jpg?_nc_cat=107&_nc_ht=scontent-frx5-1.xx&oh=d49a9311ec8afaf37cb07a9ccec51f96&oe=5CB57C44"
-              alt="Avatar photo of a team member" />
-              <CardBody>
-                <CardTitle style={{ color: '#010b15' }}><h4><Translate value="team.member5.name"/></h4></CardTitle>
-                <CardTitle style={{ color: '#010b15' }}>Github: <CardLink href="https://github.com/yana-bashtyk" target="_blank">yana-bashtyk</CardLink></CardTitle>
-                <CardTitle style={{ color: '#010b15' }}>Email: <CardLink href="mailto:yankabshtyk16@gmail.com">yankabshtyk16@gmail.com</CardLink></CardTitle>
-              </CardBody>
-            </Card>
-          </Col>
+            {
+            Object.entries(translations.team).map((item) => {
+              const [key, value] = item;
+              return (
+                <Col sm="6" md="6" lg="4" key={key}>
+                  <Card body inverse style={{ backgroundColor: '#d6effc80', marginBottom: '20px' }}>
+                    <div style={{ height: '320px', textAlign: 'center' }}>
+                      <img height="100%" src={value.img} alt="Avatar photo of a team member" />
+                    </div>
+                    <CardBody>
+                      <CardTitle style={{ color: '#010b15' }}>
+                        <h4>
+                          {value.name}
+                        </h4>
+                      </CardTitle>
+                      <CardTitle style={{ color: '#010b15' }}>Github:
+                        <CardLink href={`https://github.com/${value.github}`}
+                          target="_blank"> {value.github}
+                        </CardLink>
+                      </CardTitle>
+                      <CardTitle style={{ color: '#010b15' }}>Email:
+                        <CardLink href={`mailto:${value.mail}`}> {value.mail}</CardLink>
+                      </CardTitle>
+                    </CardBody>
+                  </Card>
+                </Col>
+              );
+            })
+          }
           </Row>
       </div>
     );
   }
 }
 
-export default HomePage;
+HomePage.propTypes = {
+  translations: PropTypes.object,
+};
+
+function mapStateToProps(state) {
+  const { i18n } = state;
+  return {
+    translations: i18n.translations[i18n.locale],
+  };
+}
+
+export default connect(mapStateToProps)(HomePage);
